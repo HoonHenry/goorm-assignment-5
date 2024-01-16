23c5d99324b7e377e99166d2e7952e63780c3c0c
HoonHenry <dreampath88@gmail.com>
2024-01-16 17:44:31 +0900

    Merge branch 'test'
    * confirm step 2-1 hpa works on the deployment kind
    * update/make sh files

--- src/rm_all.sh
+++ src/rm_all.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-cluster_arn=$(terraform output -raw cluster_arn)
-check_list=($cluster_arn)
-for i in "${check_list[@]}"; do
-    if [ -z "$i" ]; then
-      echo "data is empty"
+declare -A check_list
+check_list["cluster_arn"]=$(terraform output -raw cluster_arn)
+
+for key in "${check_list[@]}"; do
+    if [ -z "$key" ]; then
+      echo "$key is empty"
       exit 1
     fi
 done
 
+# Remove the namespace and all the resources
 kubectl delete ns assignment && \
+kubectl delete sc ebs-sc ebs-sc-mysql && \
+
+# Remove the AWS EKS cluster info from the kubeconfig file
 kubectl config unset current-context && \
+
+# Remove the AWS EKS cluster info from the kubeconfig file
 kubectl config delete-cluster ${cluster_arn} && \
 kubectl config delete-context ${cluster_arn} && \
 kubectl config delete-user ${cluster_arn} && \
+
+# Destroy the Terraform resources from AWS
 terraform destroy -auto-approve
--- src/start_k8s.sh
+++ src/start_k8s.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+terraform_cluster_arn=$(terraform output -raw cluster_arn)
+
+# if the current context of the local ~/.kube/config is not configured
+# or the current context is not identical with the Terraform cluster arn,
+# then update/switch the the cluster to the kubectl for the AWS EKS cluster
+if [[ $(kubectl config current-context) -ne 0 ]] || \
+    [[ $(kubectl config current-context) != $terraform_cluster_arn ]]; then
+    echo "Switching to cluster"
+
+    aws eks update-kubeconfig \
+        --region $(terraform output -raw region) \
+        --name $(terraform output -raw cluster_name)
+
+    echo "Cluster switched"
+fi
+echo "The current context of the local machine: $(kubectl config current-context)"
+
 #nodes=$(kubectl get node -o name | head -n 2)
 #
 #i=1
@@ -22,6 +39,6 @@ kubectl apply -k ./manifests
 # else
 #     NAME=$1
 #     echo "Creating $NAME for the ingress group name"
-#     sed -i.bak -e "s|your-ingress-group-name|$NAME|" ./wordpress-deployment.yaml && \
+#     sed -i.bak -e "s|your-ingress-group-name|$NAME|" ./manifests/wordpress/deployment.yaml && \
 #     kubectl apply -k ./manifests
 # fi
